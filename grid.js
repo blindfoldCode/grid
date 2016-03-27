dfd3120ac8777f40a28aca963891325f6885912d
Franz <pig3790@gmail.com>
2016-03-27 21:12:41 +0800

    added table row - cell

--- grid.js
+++ grid.js
@@ -5,7 +5,8 @@ for (var i = 0; i < 10; i++) {
   row.id = "row" + i;
   for (var td = 0; td < 10; td++) {
     var cell = document.createElement("td");
-    cell.className = (td <= i) ? "bg-blue" : "bg-orange"
+    cell.className = (td <= i) ? "bg-blue" : "bg-orange";
+    cell.textContent = i + " - " + td;
     row.appendChild(cell);
   }
   tableBody.appendChild(row);
