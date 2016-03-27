7c18ef61dc159ba54439751ffd9d15cd4be61112
Franz <pig3790@gmail.com>
2016-03-27 21:03:34 +0800

    added coloring for cells; also changed blue from navyblue to blueviolet

--- grid.js
+++ grid.js
@@ -5,6 +5,7 @@ for (var i = 0; i < 10; i++) {
   row.id = "row" + i;
   for (var td = 0; td < 10; td++) {
     var cell = document.createElement("td");
+    cell.className = (td <= i) ? "bg-blue" : "bg-orange"
     row.appendChild(cell);
   }
   tableBody.appendChild(row);
