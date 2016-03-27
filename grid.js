f325d13e46b66fb42d4a4cacfa866a8e69072a27
Franz <pig3790@gmail.com>
2016-03-27 21:15:01 +0800

    abstracted coloring to callback

--- grid.js
+++ grid.js
@@ -1,13 +1,19 @@
-var tableBody = document.querySelector("table");
+function run(cb) {
+  var tableBody = document.querySelector("table");
 
-for (var i = 0; i < 10; i++) {
-  var row = document.createElement("tr");
-  row.id = "row" + i;
-  for (var td = 0; td < 10; td++) {
-    var cell = document.createElement("td");
-    cell.className = (td <= i) ? "bg-blue" : "bg-orange";
-    cell.textContent = i + " - " + td;
-    row.appendChild(cell);
+  for (var i = 0; i < 10; i++) {
+    var row = document.createElement("tr");
+    row.id = "row" + i;
+    for (var td = 0; td < 10; td++) {
+      var cell = document.createElement("td");
+      cell.className = cb(td, i);
+      cell.textContent = i + " - " + td;
+      row.appendChild(cell);
+    }
+    tableBody.appendChild(row);
   }
-  tableBody.appendChild(row);
 }
+
+run(function(td, i) {
+  return (td <= i) ? "bg-blue" : "bg-orange";
+})
